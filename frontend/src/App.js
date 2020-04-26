ef10f18b1b5a2f8e6d05a0f7fd41f9db7e00324d
anonymousauthor123 <anonymousauthor123@163.com>
2020-04-26 17:47:08 +0800

    upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 import Case1 from "./Case1";
+import Case2 from "./Case2";
+import Case3 from "./Case3";
 
 const App = (props) => {
-  return <Case1 />;
+  return <Case3 />;
 };
 
 export default App;
--- frontend/src/Case2.js
+++ frontend/src/Case2.js
@@ -1,1 +1,65 @@
 import React from "react";
+
+import { useSVG_2 } from "./use-svg";
+
+const draw = (svg) => {
+  const width = 900;
+  const height = 400;
+
+  svg.attr("width", width).attr("height", height);
+
+  const nodes = [
+    { x: 30, y: 50 },
+    { x: 50, y: 80 },
+    { x: 90, y: 120 },
+  ];
+
+  svg
+    .selectAll("circle.nodes")
+    .data(nodes)
+    .enter()
+    .append("circle")
+    .attr("class", "nodes")
+    .attr("cx", function (d) {
+      return d.x;
+    })
+    .attr("cy", function (d) {
+      return d.y;
+    })
+    .attr("r", "10px")
+    .attr("fill", "black");
+
+  const links = [
+    { source: nodes[0], target: nodes[1] },
+    { source: nodes[2], target: nodes[1] },
+  ];
+
+  svg
+    .selectAll("line")
+    .data(links)
+    .enter()
+    .append("line")
+    .attr("x1", function (d) {
+      return d.source.x;
+    })
+    .attr("y1", function (d) {
+      return d.source.y;
+    })
+    .attr("x2", function (d) {
+      return d.target.x;
+    })
+    .attr("y2", function (d) {
+      return d.target.y;
+    })
+    .style("stroke", "rgb(6,120,155)");
+
+  return svg;
+};
+
+const Case2 = (props) => {
+  const container = useSVG_2(draw);
+
+  return <div ref={container}></div>;
+};
+
+export default Case2;
